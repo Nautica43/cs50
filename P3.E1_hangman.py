460115bc520b31cc47a584147c322ebaf7202608
Alex Miller <alex.miller692@gmail.com>
2025-02-15 20:41:37 -0500

    Work done on Sat. Feb 15

--- P3.E1_hangman.py
+++ P3.E1_hangman.py
@@ -4,4 +4,11 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
